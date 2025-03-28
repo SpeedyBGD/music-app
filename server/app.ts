5923d6138b5732b93b4447ec83892dfdda15a579
SpeedyBGD <manojlovic_drazen@yahoo.com>
2025-03-28 15:34:05 +0100

    cors

--- server/app.ts
+++ server/app.ts
@@ -1,22 +1,28 @@
-import dotenv from 'dotenv';
-import express, { Express } from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import musicRoutes from './routes/musicRoutes';
-import authRoutes from './routes/authRoutes';
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import musicRoutes from "./routes/musicRoutes";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 
 const app: Express = express();
 
-app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+app.use(
+  cors({
+    origin:
+      "https://music-app-git-main-manojlovic-drazens-projects.vercel.app/",
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/music', musicRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/music", musicRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
